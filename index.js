125f5d6080ded7ce2ed54e4054bf8b83ab6facd9
ChristCYMa <69599277+ChristCYMa@users.noreply.github.com>
2020-08-25 14:27:02 -0400

    updated to take player inputs

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let dice1 = document.getElementsByClassName("dice")[0];
 let dice2 = document.getElementsByClassName("dice")[1];
 var n1;
 var n2;
+let player1 = prompt("Who is Player 1?");
+let player2 = prompt("Who is Player 2?");
+
+document.getElementsByClassName("player1")[0].innerHTML = player1;
+document.getElementsByClassName("player2")[0].innerHTML =  player2;
 
 function rollDice(){
   n1 = Math.floor((Math.random()*6)+1);
@@ -9,7 +14,7 @@ function rollDice(){
   if (n1 > n2){
      dice1.src=("images/dice"+n1+".png");
      dice2.src=("images/dice"+n2+".png");
-    document.querySelector("h1").innerText = "Player 1 Wins!";
+    document.querySelector("h1").innerText = player1 + " Wins!";
   }
     else if (n1 === n2) {
        dice1.src=("images/dice"+n1+".png");
@@ -19,6 +24,6 @@ function rollDice(){
     else {
        dice1.src=("images/dice"+n1+".png");
        dice2.src=("images/dice"+n2+".png");
-      document.querySelector("h1").innerText = "Player 2 Wins!";
+      document.querySelector("h1").innerText = player2 + " Wins!";
     }
 }
